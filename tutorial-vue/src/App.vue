0a78d5ae6da3209b04ee0d03c2ec6a5cadc59cc8
razerstel <121737971+razerstel@users.noreply.github.com>
2025-02-25 20:58:17 +0100

    proyecto inicial

---
+++ tutorial-vue/src/App.vue
@@ -0,0 +1,32 @@
+<!-- App.vue -->
+<template>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Personas</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <!-- Inclusion del componente "TablaPersonas" -->
+        <tabla-personas />
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+// Importacion del componente "TablaPersonas"
+import TablaPersonas from '@/components/TablaPersonas.vue'
+// Definicion del componente Vue
+defineOptions({
+  // Nombre del componente
+  name: 'app',
+});
+</script>
+<style>
+/* Estilos globales para todos los elementos button en la aplicacion */
+button {
+  background: #009435;
+  border: 1px solid #009435;
+}
+</style>
---
+++ tutorial-vue/src/components/TablaPersonas.vue
@@ -0,0 +1,49 @@
+<!-- src/components/TablaPersonas.vue -->
+<template>
+    <!-- Contenedor principal del componente -->
+    <div id="tabla-personas">
+        <!-- Tabla para mostrar informacion de personas -->
+        <table class="table">
+            <!-- Encabezado de la tabla -->
+            <thead>
+                <tr>
+                    <!-- Columnas del encabezado -->
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <!-- Cuerpo de la tabla -->
+            <tbody>
+                <!-- datos para Jon Nieve -->
+                <tr>
+                    <td>Jon</td>
+                    <td>Nieve</td>
+                    <td>jon@email.com</td>
+                </tr>
+                <!-- datos para Tyrion Lannister -->
+                <tr>
+                    <td>Tyrion</td>
+                    <td>Lannister</td>
+                    <td>tyrion@email.com</td>
+                </tr>
+                <!-- datos para Daenerys Targaryen -->
+                <tr>
+                    <td>Daenerys</td>
+                    <td>Targaryen</td>
+                    <td>daenerys@email.com</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+<script setup>
+// Definicion del componente Vue
+defineOptions({
+    // Nombre del componente
+    name: 'tabla-personas',
+});
+</script>
+<style scoped>
+/* Estilos especificos del componente con el modificador "scoped" */
+</style>
