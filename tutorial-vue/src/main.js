be23c73084b0260d0cd870bfe12bd67ac659bfaf
razerstel <121737971+razerstel@users.noreply.github.com>
2025-03-07 17:58:44 +0100

    Update main.js

--- tutorial-vue/src/main.js
+++ tutorial-vue/src/main.js
@@ -3,11 +3,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import './assets/main.css'
 
+// Importaciones correctas de Bootstrap
+import 'bootstrap/dist/js/bootstrap.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
 
 app.mount('#app')
 
-import "../node_modules/bootstrap/dist/js/bootstrap.js"
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
