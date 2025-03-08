1d87fd0b26c46dd54e096d0119cfa64299ea8d2b
razerstel <121737971+razerstel@users.noreply.github.com>
2025-03-08 20:29:35 +0100

    .env developent y production

--- tutorial-vue/src/main.js
+++ tutorial-vue/src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+//const API_URL = import.meta.env.VITE_DJANGOURL;
+const API_URL = import.meta.env.VITE_DJANGOURL;
+console.log("Backend URL:", API_URL);
 
 const app = createApp(App)
 
@@ -8,5 +14,3 @@ app.use(createPinia())
 
 app.mount('#app')
 
-import "../node_modules/bootstrap/dist/js/bootstrap.js";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
