dd9e8e5657d94f46775ff0a434131feecb68dad8
razerstel <121737971+razerstel@users.noreply.github.com>
2025-03-09 13:44:34 +0100

    .

--- persona/build.sh
+++ persona/build.sh
@@ -1,4 +1,4 @@
 pip3 install -r ../requirements2.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
-DJANGO_SUPERUSER_PASSWORD=alumnodb python manage.py createsuperuser --username alumnodb --email alumnodb@alumnodb.com --noinput
+#DJANGO_SUPERUSER_PASSWORD=alumnodb python manage.py createsuperuser --username alumnodb --email alumnodb@alumnodb.com --noinput
